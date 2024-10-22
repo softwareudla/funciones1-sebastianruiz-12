9ef7d149e3d85b05937630166845144522d0cc1d
sebastianruiz-12 <sabesriz88@gmail.com>
2024-10-22 13:55:09 -0500

    commit 17

--- funciones.c
+++ funciones.c
@@ -2,16 +2,15 @@
 #include <string.h>
 #include "funciones.h"
 
-// Función para ingresar productos
-void ingresarProductos(char nombres[10][50], float precios[10], int *numProductos) {
+void ingresarProductos(char nombres[][MAX_NOMBRE], float precios[], int *numProductos) {
     printf("¿Cuantos productos desea ingresar (max %d)? ", MAX_PRODUCTOS);
     scanf("%d", numProductos);
-
+    
     if (*numProductos > MAX_PRODUCTOS) {
         *numProductos = MAX_PRODUCTOS;
-        printf("Se limitará a %d productos.\n", MAX_PRODUCTOS);
+        printf("Se limitara a %d productos.\n", MAX_PRODUCTOS);
     }
-
+    
     for (int i = 0; i < *numProductos; i++) {
         printf("\nProducto %d:\n", i + 1);
         printf("Nombre: ");
@@ -21,8 +20,7 @@ void ingresarProductos(char nombres[10][50], float precios[10], int *numProducto
     }
 }
 
-// Función para calcular el total de los precios
-float calcularTotal(float precios[10], int numProductos) {
+float calcularTotal(float precios[], int numProductos) {
     float total = 0;
     for (int i = 0; i < numProductos; i++) {
         total += precios[i];
@@ -30,11 +28,10 @@ float calcularTotal(float precios[10], int numProductos) {
     return total;
 }
 
-// Función para encontrar los productos más caro y más barato
-void encontrarExtremos(float precios[10], int numProductos, char nombres[10][50], 
-                       char nombreMasCaro[50], char nombreMasBarato[50]) {
+void encontrarExtremos(float precios[], int numProductos, char nombres[][MAX_NOMBRE], 
+                char *nombreMasCaro, char *nombreMasBarato) {
     int indexMasCaro = 0, indexMasBarato = 0;
-
+    
     for (int i = 1; i < numProductos; i++) {
         if (precios[i] > precios[indexMasCaro]) {
             indexMasCaro = i;
@@ -43,19 +40,17 @@ void encontrarExtremos(float precios[10], int numProductos, char nombres[10][50]
             indexMasBarato = i;
         }
     }
-
+    
     strcpy(nombreMasCaro, nombres[indexMasCaro]);
     strcpy(nombreMasBarato, nombres[indexMasBarato]);
 }
 
-// Función para calcular el promedio de los precios
-float calcularPromedio(float precios[10], int numProductos) {
+float calcularPromedio(float precios[], int numProductos) {
     if (numProductos == 0) return 0;
     return calcularTotal(precios, numProductos) / numProductos;
 }
 
-// Función para buscar un producto
-int buscarProducto(char nombres[10][50], int numProductos, const char *buscar) {
+int buscarProducto(char nombres[][MAX_NOMBRE], int numProductos, const char *buscar) {
     for (int i = 0; i < numProductos; i++) {
         if (strcmp(nombres[i], buscar) == 0) {
             return i;
@@ -64,7 +59,11 @@ int buscarProducto(char nombres[10][50], int numProductos, const char *buscar) {
     return -1;
 }
 
-// Función para mostrar el inventario de productos
-void mostrarInventario(char nombres[10][50], float precios[10], int numProductos) {
+void mostrarInventario(char nombres[][MAX_NOMBRE], float precios[], int numProductos) {
     printf("\nInventario actual:\n");
-    printf("%-30s %s\n", "Producto");
+    printf("%-30s %s\n", "Producto", "Precio");
+    printf("----------------------------------------\n");
+    for (int i = 0; i < numProductos; i++) {
+        printf("%-30s %.2f\n", nombres[i], precios[i]);
+    }
+} FUNCIONES.C
