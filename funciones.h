5c14a6dea8b0114f9b4b149c2ce262aeff6e8469
sebastianruiz-12 <sabesriz88@gmail.com>
2024-10-22 15:26:53 -0500

    commit 34

--- funciones.h
+++ funciones.h
@@ -11,7 +11,7 @@ float calcularTotal(float precios[MAX_PRODUCTOS], int numProductos);
 void encontrarExtremos(float precios[MAX_PRODUCTOS], int numProductos, char nombres[MAX_PRODUCTOS][MAX_NOMBRE], 
                        char nombreMasCaro[MAX_NOMBRE], char nombreMasBarato[MAX_NOMBRE]);
 float calcularPromedio(float precios[MAX_PRODUCTOS], int numProductos);
-int buscarProducto(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int numProductos, char buscar);
+int buscarProducto(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int numProductos, char buscar[MAX_NOMBRE]);
 void mostrarInventario(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], float precios[MAX_PRODUCTOS], int numProductos);
 
 #endif
